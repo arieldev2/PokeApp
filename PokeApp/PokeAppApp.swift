d291cd3ff226045df9a4170d4c119b10ca534234
Ariel Ortiz <arielortiz.dev@gmail.com>
2025-08-27 15:09:00 -0700

    Initial Commit

---
+++ PokeApp/PokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAppApp.swift
+//  PokeApp
+//
+//  Created by Ariel Ortiz on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct PokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
